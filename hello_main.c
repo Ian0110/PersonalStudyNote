f32c0f087aa1e52ebc944d493a94bc443f737318
Ian0110 <xiaoduan0110@163.com>
2023-08-17 17:20:27 +0800

    use wirte() wirte buf to stdout

--- hello_main.c
+++ hello_main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 int main(void)
 {
 	printf("hello main\n");
 	char *str = "branch test modify this file and output\n";
 	
-	int fd = write(1,str,sizeof(str));
+	int len = write(STDOUT_FILENO,str,strlen(str)); 
+	printf("string length is %d\n",len);
 	return 0;
 }
