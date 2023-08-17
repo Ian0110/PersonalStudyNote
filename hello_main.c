b9e22b701b11573bd613037349d5aefc54066b48
Ian0110 <xiaoduan0110@163.com>
2023-08-17 17:25:27 +0800

    branch main commit 8/17/17:25

--- hello_main.c
+++ hello_main.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-<<<<<<< HEAD
 
-=======
 #include <fcntl.h>
->>>>>>> test
 int main(void)
 {
 	int i;
 	printf("hello main\n");
-<<<<<<< HEAD
 	char buf[64] = "how i can't get the afternoon out of my mind";
 	for(i = 0;i<sizeof(buf);i++)
 		putchar(buf[i]);
 	putchar('\n');
-=======
+
 	char *str = "branch test modify this file and output\n";
-	
 	int fd = write(1,str,sizeof(str));
->>>>>>> test
 	return 0;
 }
