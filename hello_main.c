f05057bb0cc282c8945bc87e310264575b28e771
Ian0110 <xiaoduan0110@163.com>
2023-08-17 16:54:31 +0800

    a little bit modify to the c file

--- hello_main.c
+++ hello_main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <fcntl.h>
 int main(void)
 {
 	printf("hello main\n");
+	char *str = "branch test modify this file and output\n";
+	
+	int fd = write(1,str,sizeof(str));
 	return 0;
 }
