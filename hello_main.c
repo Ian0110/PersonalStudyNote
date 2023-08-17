627dd6f95cc9636a4fddb8e7407eef7eafee89be
Ian0110 <xiaoduan0110@163.com>
2023-08-17 17:03:52 +0800

    let hello_main.c say something else and complie it into main,try to run it

--- hello_main.c
+++ hello_main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
+	int i;
 	printf("hello main\n");
+	char buf[64] = "how i can't get the afternoon out of my mind";
+	for(i = 0;i<sizeof(buf);i++)
+		putchar(buf[i]);
+	putchar('\n');
 	return 0;
 }
